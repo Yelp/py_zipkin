d779de84105f8195f87dfffabe427f7fc9bffd46
Kaisen Chen <kaisen@yelp.com>
2016-09-16 11:39:26 -0700

    Bump to v0.1.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,3 @@
-0.1.0 (2016-07-05)
+0.1.1 (2016-07-05)
 ------------------
 - First py_zipkin version with context manager/decorator functionality.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 setup(
     name='py_zipkin',
