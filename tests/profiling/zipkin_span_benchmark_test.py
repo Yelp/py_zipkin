2c9249f181fa7808090c7210cb47450852fe3d8c
Mayank Sindwani <msindwan@yelp.com>
2018-03-06 13:36:41 -0800

    Refactor span benchmarks for clarity.

--- tests/profiling/zipkin_span_benchmark_test.py
+++ tests/profiling/zipkin_span_benchmark_test.py
@@ -1,121 +1,134 @@
 import pytest
 import mock
-import socket
 import py_zipkin.zipkin as zipkin
 
 
-class UDPTransportHandler(object):
-
-    def __init__(self):
-        self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.recv_socket.bind(('', 0))
-
-    def __bool__(self):
-        return True
-
-    def __nonzero__(self):
-        return True
-
-    def __call__(self, message):
-        self.send_socket.sendto(message, self.recv_socket.getsockname())
-
-
-class TCPTransportHandler(object):
-
-    def __init__(self):
-        self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.recv_socket.bind(('', 0))
-        self.recv_socket.listen(1)
-        self.send_socket.connect(self.recv_socket.getsockname())
-
-    def __bool__(self):
-        return True
-
-    def __nonzero__(self):
-        return True
-
-    def __call__(self, message):
-        self.send_socket.send(message)
-
-
-@pytest.mark.parametrize('transport_handler', [
-    mock.Mock(),
-    UDPTransportHandler(),
-    TCPTransportHandler(),
-])
-@pytest.mark.parametrize('firehose_handler', [
-    None,
-    UDPTransportHandler(),
-    TCPTransportHandler(),
-])
-@pytest.mark.parametrize('sample_rate', [0.15, 100])
-def test_zipkin_span_thread_local(
-    benchmark,
-    transport_handler,
-    firehose_handler,
-    sample_rate,
-):
-    def benchmark_zipkin_span():
-        context = zipkin.zipkin_span(
+def _create_root_span(is_sampled, firehose_enabled):
+    return (), {
+        'root_span': zipkin.zipkin_span(
             service_name='my_service',
             span_name='my_span_name',
-            transport_handler=transport_handler,
-            firehose_handler=firehose_handler,
+            transport_handler=mock.Mock(),
+            firehose_handler=mock.Mock() if firehose_enabled else None,
             port=42,
-            sample_rate=sample_rate,
+            sample_rate=0 if not is_sampled else 100,
         )
-        context.start()
-
-    benchmark(benchmark_zipkin_span)
+    }
 
 
-@pytest.mark.parametrize('transport_handler', [
-    mock.Mock(),
-    UDPTransportHandler(),
-    TCPTransportHandler(),
-])
-@pytest.mark.parametrize('firehose_handler', [
-    None,
-    UDPTransportHandler(),
-    TCPTransportHandler(),
-])
-@pytest.mark.parametrize('sample_rate', [0.15, 100])
-@pytest.mark.parametrize('num_spans', [1, 100, 1000])
-def test_zipkin_span_logging(
-    benchmark,
-    transport_handler,
-    firehose_handler,
-    sample_rate,
-    num_spans,
-):
-    def build_trace():
-        context = zipkin.zipkin_span(
+def _start_zipkin_trace(is_sampled, firehose_enabled, num_child_spans):
+    _, args = _create_root_span(is_sampled, firehose_enabled)
+    args['root_span'].start()
+    for _ in range(num_child_spans):
+        with zipkin.zipkin_span(
             service_name='my_service',
-            span_name='my_span_name',
-            transport_handler=transport_handler,
-            firehose_handler=firehose_handler,
-            port=42,
-            sample_rate=sample_rate,
-        )
-        context.start()
-        for _ in range(num_spans):
-            with zipkin.zipkin_span(
-                service_name='my_service',
-                span_name='my_span_name',
-            ):
-                pass
+            span_name='my_child_span_name',
+        ):
+            pass
 
-        return (), {'context': context}
-
-    def benchmark_zipkin_span(context):
-        context.stop()
-
-    benchmark.pedantic(benchmark_zipkin_span, setup=build_trace, rounds=100)
+    return (), args
 
 
-@pytest.mark.parametrize('use_128', [False, True])
+def _start_and_stop_zipkin_trace(is_sampled, firehose_enabled, num_child_spans):
+    _, args = _start_zipkin_trace(is_sampled, firehose_enabled, num_child_spans)
+    args['root_span'].stop()
+
+
+@pytest.mark.parametrize(
+    'firehose_enabled',
+    [False, True],
+    ids=["firehose_enabled=False", "firehose_enabled=True"],
+)
+@pytest.mark.parametrize(
+    'is_sampled',
+    [False, True],
+    ids=["is_sampled=False", "is_sampled=True"],
+)
+def test_creating_span(benchmark, firehose_enabled, is_sampled):
+    benchmark(
+        _create_root_span,
+        firehose_enabled=firehose_enabled,
+        is_sampled=is_sampled,
+    )
+
+
+@pytest.mark.parametrize(
+    'firehose_enabled',
+    [False, True],
+    ids=["firehose_enabled=False", "firehose_enabled=True"],
+)
+@pytest.mark.parametrize(
+    'is_sampled',
+    [False, True],
+    ids=["is_sampled=False", "is_sampled=True"],
+)
+def test_starting_span_context(benchmark, firehose_enabled, is_sampled):
+    benchmark.pedantic(
+        lambda root_span: root_span.start(),
+        setup=lambda: _create_root_span(
+            is_sampled=is_sampled,
+            firehose_enabled=firehose_enabled,
+        ),
+        rounds=50,
+    )
+
+
+@pytest.mark.parametrize(
+    'firehose_enabled',
+    [False, True],
+    ids=["firehose_enabled=False", "firehose_enabled=True"],
+)
+@pytest.mark.parametrize(
+    'num_child_spans',
+    [100, 1000],
+    ids=["num_child_spans=100", "num_child_spans=1000"],
+)
+@pytest.mark.parametrize(
+    'is_sampled',
+    [False, True],
+    ids=["is_sampled=False", "is_sampled=True"],
+)
+def test_logging_spans(benchmark, firehose_enabled, num_child_spans, is_sampled):
+    benchmark.pedantic(
+        lambda root_span: root_span.stop(),
+        setup=lambda: _start_zipkin_trace(
+            is_sampled=is_sampled,
+            firehose_enabled=firehose_enabled,
+            num_child_spans=num_child_spans,
+        ),
+        rounds=50,
+    )
+
+
+@pytest.mark.parametrize(
+    'firehose_enabled',
+    [False, True],
+    ids=["firehose_enabled=False", "firehose_enabled=True"],
+)
+@pytest.mark.parametrize(
+    'num_child_spans',
+    [100, 1000],
+    ids=["num_child_spans=100", "num_child_spans=1000"],
+)
+@pytest.mark.parametrize(
+    'is_sampled',
+    [False, True],
+    ids=["is_sampled=False", "is_sampled=True"],
+)
+def test_zipkin_span(benchmark, firehose_enabled, num_child_spans, is_sampled):
+    benchmark(
+        _start_and_stop_zipkin_trace,
+        is_sampled=is_sampled,
+        firehose_enabled=firehose_enabled,
+        num_child_spans=num_child_spans,
+    )
+
+
+@pytest.mark.parametrize(
+    'use_128',
+    [False, True],
+    ids=["use_128=False", "use_128=True"],
+)
 def test_create_attrs_for_span(benchmark, use_128):
     benchmark(
         zipkin.create_attrs_for_span,
