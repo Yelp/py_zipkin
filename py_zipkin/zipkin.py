22bc1ecb3313cd28e44886db2ea0f0486e917417
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-04-10 09:14:32 +0800

    Update zipkin.py

--- py_zipkin/zipkin.py
+++ py_zipkin/zipkin.py
@@ -3,9 +3,7 @@ import functools
 import logging
 import random
 import time
-
-import six
-
+import sys
 from collections import namedtuple
 
 from py_zipkin import Encoding
@@ -488,7 +486,7 @@ class zipkin_span(object):
         # Add the error annotation if an exception occurred
         if any((_exc_type, _exc_value, _exc_traceback)):
             error_msg = u'{0}: {1}'.format(_exc_type.__name__, _exc_value)
-            if six.PY3:
+            if int(sys.version[0:1]) >= 3:
                 self.update_binary_annotations({
                     ERROR_KEY: error_msg,  # pragma: no cover
                 })
