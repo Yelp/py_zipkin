0261f758889e49dfef998dcf7d752107bca37640
Ben Bariteau <bariteau@yelp.com>
2022-08-26 16:34:15 -0700

    add type annotations to py_zipkin/instrumentations/python_threads.py

--- py_zipkin/instrumentations/python_threads.py
+++ py_zipkin/instrumentations/python_threads.py
@@ -7,13 +7,13 @@ _orig_Thread_start = threading.Thread.start
 _orig_Thread_run = threading.Thread.run
 
 
-def _Thread_pre_start(self):
+def _Thread_pre_start(self) -> None:
     self._orig_tracer = None
     if storage.has_default_tracer():
         self._orig_tracer = storage.get_default_tracer().copy()
 
 
-def _Thread_wrap_run(self, actual_run_fn):
+def _Thread_wrap_run(self, actual_run_fn) -> None:
     # This executes in the new OS thread
     if self._orig_tracer:
         # Inject our copied Tracer into our thread-local-storage
@@ -27,7 +27,7 @@ def _Thread_wrap_run(self, actual_run_fn):
             del self._orig_tracer
 
 
-def patch_threading():  # pragma: no cover
+def patch_threading() -> None:  # pragma: no cover
     """Monkey-patch threading module to work better with tracing."""
 
     def _new_start(self):
@@ -37,10 +37,10 @@ def patch_threading():  # pragma: no cover
     def _new_run(self):
         _Thread_wrap_run(self, partial(_orig_Thread_run, self))
 
-    threading.Thread.start = _new_start
-    threading.Thread.run = _new_run
+    threading.Thread.start = _new_start  # type: ignore[assignment]
+    threading.Thread.run = _new_run  # type: ignore[assignment]
 
 
-def unpatch_threading():  # pragma: no cover
-    threading.Thread.start = _orig_Thread_start
-    threading.Thread.run = _orig_Thread_run
+def unpatch_threading() -> None:  # pragma: no cover
+    threading.Thread.start = _orig_Thread_start  # type: ignore[assignment]
+    threading.Thread.run = _orig_Thread_run  # type: ignore[assignment]
